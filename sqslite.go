8518fb9b61fbd0dba44e595eeccd78b7b7bddcfe
Eric Ashman <eric@ashman.com>
2019-04-09 18:40:12 -0400

    dist

--- sqslite.go
+++ sqslite.go
@@ -1,29 +1,32 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/jessevdk/go-flags"
 )
-
-// const (
-// 	QueueUrl = "https://sqs.us-east-1.amazonaws.com/385697007281/sync-md.fifo"
-// )
 
 func main() {
 	access := os.Getenv("AWS_ACCESS_KEY_ID")
 	secret := os.Getenv("AWS_SECRET_ACCESS_KEY")
 	region := os.Getenv("AWS_REGION")
-	qUrl := flag.String("q", "", "queue url")
 
-	flag.Parse()
+	var opts struct {
+		File  string `short:"f" long:"file" description:"A file" value-name:"FILE"`
+		Queue string `short:"q" long:"queue" description:"Queue to User" value-name:"QUEUE" default:"https://sqs.us-east-1.amazonaws.com/385697007281/sync-md.fifo"`
+	}
 
+	flags.Parse(&opts)
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+
+	fmt.Println(currentTime, " - FILE - name: ", strings.TrimSpace(opts.File))
 	// Check required environment variables
 	if access == "" {
 		panic("AWS_ACCESS_KEY_ID is undefined")
@@ -37,45 +40,24 @@ func main() {
 		panic("AWS_REGION is undefined")
 	}
 
-	var queue_url string
-	if *qUrl == "" {
-		queue_url = string("https://sqs.us-east-1.amazonaws.com/385697007281/sync-md.fifo")
-	} else {
-		queue_url = *qUrl
-	}
-
-	fmt.Println("1")
-
-	// B is the filename
-	var b []byte
-	var err error
-	b, err = ioutil.ReadAll(os.Stdin)
-	fmt.Println("2")
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("3")
-
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String(region),
 		Credentials: credentials.NewStaticCredentials(access, secret, ""),
 		MaxRetries:  aws.Int(2),
 	})
 
-	svc := sqs.New(sess, aws.NewConfig().WithLogLevel(aws.LogDebugWithHTTPBody))
-	//svc := sqs.New(sess)
+	//svc := sqs.New(sess, aws.NewConfig().WithLogLevel(aws.LogDebugWithHTTPBody))
+	svc := sqs.New(sess)
 
 	// Send message
 	send_params := &sqs.SendMessageInput{
-		MessageBody:    aws.String(string(b)),
-		QueueUrl:       aws.String(queue_url),
+		MessageBody:    aws.String(strings.TrimSpace(opts.File)),
+		QueueUrl:       aws.String(opts.Queue),
 		MessageGroupId: aws.String("1"),
 	}
 	send_resp, err := svc.SendMessage(send_params)
 	if err != nil {
-		fmt.Println("[Error message]\n", err)
+		fmt.Println(currentTime, " - ERROR - ", err)
 	}
-	fmt.Println("[Sent message]\n", send_resp)
+	fmt.Println(currentTime, " - SENT - msgid: ", send_resp.MessageId)
 }
